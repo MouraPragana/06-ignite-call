568624beb5f2bf036288f7fd64039d7c0e52fadd
Mateus Pragana <mateuspragana@gmail.com>
2023-09-02 00:01:08 -0300

    feat: finalizando aula: Valor inicial do nome do usuário

--- src/pages/home/components/ClaimUserNameForm/index.tsx
+++ src/pages/home/components/ClaimUserNameForm/index.tsx
@@ -4,6 +4,7 @@ import { Form, FormAnnotation } from './styles'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
 
 const claimUserNameFormSchema = z.object({
   username: z
@@ -21,13 +22,16 @@ export function ClaimUserNameForm() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
   } = useForm<ClaimUserNameFormData>({
     resolver: zodResolver(claimUserNameFormSchema),
   })
 
+  const router = useRouter()
+
   async function handleClaimUserName(data: ClaimUserNameFormData) {
-    console.log(data)
+    const { username } = data
+    await router.push(`/register?username=${username}`)
   }
 
   return (
@@ -40,7 +44,7 @@ export function ClaimUserNameForm() {
           crossOrigin={''}
           {...register('username')}
         />
-        <Button size="sm" type="submit">
+        <Button size="sm" type="submit" disabled={isSubmitting}>
           Reservar
           <ArrowRight />
         </Button>
