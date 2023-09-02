06bd50d5c2ace34ff2465e37d9f75c977896d8cb
Mateus Pragana <mateuspragana@gmail.com>
2023-09-01 21:32:57 -0300

    feat: finalizando aula: Utilizando React Hook Form

--- src/pages/home/components/ClaimUserNameForm/index.tsx
+++ src/pages/home/components/ClaimUserNameForm/index.tsx
@@ -1,15 +1,31 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
 import { Form } from './styles'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const claimUserNameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUserNameFormData = z.infer<typeof claimUserNameFormSchema>
 
 export function ClaimUserNameForm() {
+  const { register, handleSubmit } = useForm<ClaimUserNameFormData>()
+  // FormEvent<HTMLFormElement>
+
+  async function handleClaimUserName(data: ClaimUserNameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
+    <Form as="form" onSubmit={handleSubmit(handleClaimUserName)}>
       <TextInput
         size="sm"
         prefix="ignite.com/"
         placeholder="seu-usuario"
         crossOrigin={''}
+        {...register('username')}
       />
       <Button size="sm" type="submit">
         Reservar
